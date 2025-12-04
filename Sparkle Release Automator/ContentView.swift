5e0c8d0d792ecf09759c63fd6656c86d47f83ab9
uecoding <127999507+uecoding@users.noreply.github.com>
2025-12-04 23:30:59 +0100

    Initial Commit

---
+++ Sparkle Release Automator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sparkle Release Automator
+//
+//  Created by Umut on 04/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sparkle Release Automator/Sparkle_Release_AutomatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sparkle_Release_AutomatorApp.swift
+//  Sparkle Release Automator
+//
+//  Created by Umut on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Sparkle_Release_AutomatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
